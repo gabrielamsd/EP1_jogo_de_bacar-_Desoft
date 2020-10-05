0abe941c579e2c909b3a713c3a7c0f76ba2292e1
Gabriela de Mesquita Sampaio Duarte <gabrielad4@al.insper.edu.br>
2020-10-05 11:14:12 -0300

    versão 4 - implementando regras de recebimento de valor e comissão

--- EP1.py
+++ EP1.py
@@ -1,47 +1,68 @@
 # n = índice das cartas para recuperar na lista
 # append = aplica na lista os novos elementos
 # regras da terceira carta para o banco implemetadas
+# tirar as cartas que já foram sorteadas na hora de sortear as próximas
+# trava na função na segunda vez que roda
 import random
 # lista de cartas e lista de valores das cartas 
-# add *4 pra ser um baralho completo de 52 cartas
-cartas = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] * 4
+# add *8 pra ser oito baralho completo de 52 cartas - contador adicionado que limita o número de cartas para 4 por baralho, 32 cartas de cada símbolo.
+def sorteio_de_cartas(cartas, contador, mão, n_cartas):
+    i = 0
+    while i< n_cartas: 
+        índice_carta = random.randint(0,12)
+        carta = cartas[índice_carta]
+        if contador[n_cartas] > 0:
+            mão.append(carta)
+            contador[índice_carta] -= 1
+            i += 1
+        
+    return mão
+# add comissão para 8 baralhos
+# implementando regras de valores para diferentes apostas
+def comissão(aposta, valor_da_aposta, fichas1):
+    if aposta == 'B':
+        fichas1 += valor_da_aposta * 0.9894 
+    elif aposta == 'E':
+        fichas1 += valor_da_aposta * 0.8564 * 8
+    elif aposta == 'J':
+        fichas1 += valor_da_aposta * 0.9876 * 0.95
+    return fichas1
+
+cartas = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] 
 valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0]
 fichas1 = 100
+contador = [32]*len(cartas)  # 32 cartas de cada pois em um baralho existem 4 naipes. com 8 baralhos o limite fica 32 cartas para cada símbolo
 # por enquanto só um jogador e o banco
 # adicionado while e condição de validade para a interação
 jogando = True
 while jogando:
     venceu = True
-    jogador1 = []
+    jogador1 = [] # listas vazias para as cartas serem adicionadas
     banco = []
     print('Você está começando a rodada com {} fichas'.format(fichas1))
-    inválido = True
+    inválido = True # invalida as situações em que o jogo não poderia acontecer. evita apostar mais do que poderia e escrever termos sem sentido
     while inválido:
-        aposta = input('Qual é sua aposta? Digite "B" para banco, "E" para empate e "J" para jogador. Para sair do jogo aperte S. ') 
+        aposta = input('Qual é sua aposta? Digite "B" para banco, "E" para empate e "J" para jogador. Para sair do jogo aperte S. ') # padronização de apenas uma letra maiúscula para não dificultar a escrita
         if aposta != 'B' and aposta != 'E' and aposta != 'J':
             print('Aposta inválida!')
         elif aposta == 'S':
             jogando = False
+            print('Obrigado por jogar!')
         else:
             inválido = False
     inválido = True
     while inválido:    
-        valor_da_aposta = int(input('Quanto você aposta? '))
+        valor_da_aposta = int(input('Quanto você aposta? Digite apenas números por favor '))
         if valor_da_aposta > fichas1:
             print('Você não possui essa quantidade de fichas. Seu limite é {}'.format(fichas1))
         else:
             inválido = False
-
+    # sorteando duas cartas para cada um
     # adicionado falar as cartas selecionadas para organizar a interação
-    i = 0
-    while i < 2:
-        n_jogador1 = random.randint(0,12)
-        n_banco = random.randint(0,12)
-        carta_jogador1 = cartas[n_jogador1]
-        carta_banco = cartas[n_banco]
-        jogador1.append(carta_jogador1)
-        banco.append(carta_banco)
-        i += 1
+    # adicionada a função para substituir o parágrafo
+    print('Sorteando...')
+    jogador1 = sorteio_de_cartas(cartas, contador, jogador1, 2)
+    banco = sorteio_de_cartas(cartas, contador, banco, 2)
     print('Suas cartas:', jogador1)
     print('Cartas do banco:', banco)
     
@@ -68,11 +89,9 @@ while jogando:
     # ...nas duas somas: 
     sorteia_banco = False
     if soma_das_cartas1 <= 5 and soma_das_cartasb <= 6:
-        n_jogador1 = random.randint(0,12)
-        carta_jogador1 = cartas[n_jogador1]
-        jogador1.append(carta_jogador1)
-        índice_valor1 = cartas.index(jogador1[2])
-        soma_das_cartas1 += valores[índice_valor1]
+        jogador1 = sorteio_de_cartas(cartas, contador, jogador1, 1)
+        soma_das_cartas1 += valores[cartas.index(jogador1[2])]
+
         if soma_das_cartas1 > 9:
             soma_das_cartas1 -= 10
         print('As somas das suas cartas e das cartas do banco foram ambas muito pequenas. Sorteando uma terceira carta para o jogador 1 e avaliando se o banco receberá mais uma carta...')
@@ -95,11 +114,8 @@ while jogando:
       
     # ...na soma das cartas jogador1:
     if soma_das_cartas1 <= 5 and (soma_das_cartasb >= 6 and soma_das_cartasb <= 7):
-        n_jogador1 = random.randint(0,12)
-        carta_jogador1 = cartas[n_jogador1]
-        jogador1.append(carta_jogador1)
-        índice_valor1 = cartas.index(jogador1[2])
-        soma_das_cartas1 += valores[índice_valor1]  
+        jogador1 = sorteio_de_cartas(cartas, contador, jogador1, 1) 
+        soma_das_cartas1 += valores[cartas.index(jogador1[2])] 
         if soma_das_cartas1 > 9:
             soma_das_cartas1 -= 10 
         print('A soma das suas cartas foi menor que 5. Nova soma das cartas:', soma_das_cartas1)
@@ -109,15 +125,11 @@ while jogando:
         print('A soma das cartas do banco foi menor que 5')
         
     if sorteia_banco == True:
-        n_banco = random.randint(0,12)
-        carta_banco = cartas[n_banco]
-        banco.append(carta_banco)
-        índice_valorb = cartas.index(banco[2])
-        soma_das_cartasb += valores[índice_valorb]   
+        banco = sorteio_de_cartas(cartas, contador, banco, 1) 
+        soma_das_cartasb += valores[cartas.index(banco[2])]  
         if soma_das_cartasb > 9:
             soma_das_cartasb -= 10
-        if soma_das_cartas1 > 9:
-            soma_das_cartas1 -= 10 
+        
         print('Nova soma das cartas do banco:', soma_das_cartasb) 
 
     # adicionado vencer = True para facilitar e diminuir o programa
@@ -187,9 +199,9 @@ while jogando:
                 venceu = False
 
     if venceu == True:
-        fichas1 += valor_da_aposta
+        fichas1 = comissão(aposta, valor_da_aposta, fichas1)
         print('Você ganhou essa rodada')
-        print('Você tem {} fichas'.format(fichas1))
+        print('Você tem {0:.2f} fichas'.format(fichas1)) # conversei com o andrew sobre o número de casas decimais e ele disse que uma boa ideia seria reduzir pra duas 
 
     elif venceu == False:
         fichas1 -= valor_da_aposta
